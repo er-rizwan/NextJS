5fcf403b8d5cdad9ffef23dcdc6f7c3cae095f8f
Rizwan Ahmad Ansari <rizwanhim@gmail.com>
2024-06-09 08:40:16 +0530

    login and about page added

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -11,10 +12,22 @@ export default function Home() {
   const Apple=()=>{
    setName("Bhutoo")
   } 
+
+  const InnerComp=()=>{
+    return(
+      <h1>Inner Component</h1>
+    )
+  }
+
+
   
   return (
     <main className={styles.main}>
       <h1>{name}</h1>
+      <InnerComp/>
+      <Link href="/Login">Go to Login Page</Link>
+      <Link href="/About">Go to About Page</Link>
+      {InnerComp()}
       <button onClick={()=>Apple()}>Click Me</button>
     </main>
   );
