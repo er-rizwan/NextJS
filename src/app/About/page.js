635f9551f6f8e49767484c991f9014ef16be6b52
Rizwan Ahmad Ansari <rizwanhim@gmail.com>
2024-06-09 08:45:50 +0530

    Linlk

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,8 +1,11 @@
-
+import Link from "next/link";
 
 const About=()=>{
     return(
+       <div>
         <h1>About Page</h1>
+        <Link href='/'>Goto Home Page</Link>
+        </div>
     )
 }
 
--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
 
 
 const Login=()=>{
     return(
+        <div>
         <h1>Login Page</h1>
+        <Link href='/'>Goto Home Page</Link>
+        </div>
     )
 }
 
