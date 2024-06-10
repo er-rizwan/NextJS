24f2820207aea41c91f252bd624e66f73b0c59c0
Rizwan Ahmad Ansari <rizwanhim@gmail.com>
2024-06-10 06:33:39 +0530

    added new pages

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
 import Link from "next/link";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
-
+  const router= useRouter();
   const [name, setName]=useState('Rizwan');
 
   const Apple=()=>{
@@ -19,14 +20,18 @@ export default function Home() {
     )
   }
 
+  const Navig =(rName)=>{
+    router.push(rName)
+  }
 
   
   return (
     <main className={styles.main}>
       <h1>{name}</h1>
-      <InnerComp/>
       <Link href="/Login">Go to Login Page</Link>
       <Link href="/About">Go to About Page</Link>
+      <button onClick={()=>Navig('/Login')}>Go To Login</button>
+      <button onClick={()=>Navig('/About')}>Go To About</button>
       {InnerComp()}
       <button onClick={()=>Apple()}>Click Me</button>
     </main>
